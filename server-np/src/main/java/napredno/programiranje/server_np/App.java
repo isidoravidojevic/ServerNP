476cecf3a3c5bc04ae1a3771a0203b061525ace3
isidoravidojevic <isidora.vid@gmail.com>
2023-09-05 16:10:16 +0200

    Created project.

---
+++ server-np/src/main/java/napredno/programiranje/server_np/App.java
@@ -0,0 +1,13 @@
+package napredno.programiranje.server_np;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
